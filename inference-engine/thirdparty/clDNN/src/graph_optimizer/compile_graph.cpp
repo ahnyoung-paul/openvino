e537d82d6b30300f171763191c0d462ea2072f1e
Ahn, Paul Y <paul.y.ahn@intel.com>
2021-08-03 12:08:30 +0900

    Add observer

--- inference-engine/thirdparty/clDNN/src/graph_optimizer/compile_graph.cpp
+++ inference-engine/thirdparty/clDNN/src/graph_optimizer/compile_graph.cpp
@@ -10,6 +10,8 @@
 #include "program_node.h"
 #include "cldnn/runtime/engine.hpp"
 #include "runtime/cldnn_itt.hpp"
+#include "tbb/task_scheduler_observer.h"
+#include "tbb/task_arena.h"
 #include <iostream>
 #include <cmath>
 #include <iomanip>
@@ -19,42 +21,118 @@
 #include <tbb/blocked_range.h>
 #endif
 
+#if defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
+#endif
+
 using namespace cldnn;
 
-// struct Observer: public tbb::task_scheduler_observer {
-//     CpuSet              _mask;
-//     int                 _ncpus                  = 0;
-//     int                 _threadBindingStep      = 0;
-//     int                 _offset                 = 0;
-//     tbb::task_arena&    _my_arena;
-//     Observer(tbb::task_arena&    arena,
-//                 CpuSet              mask,
-//                 int                 ncpus,
-//                 const int           streamId,
-//                 const int           threadsPerStream,
-//                 const int           threadBindingStep,
-//                 const int           threadBindingOffset) :
-//         tbb::task_scheduler_observer(arena),
-//         _my_arena(arena),
-//         _mask{std::move(mask)},
-//         _ncpus(ncpus),
-//         _threadBindingStep(threadBindingStep),
-//         _offset{streamId * threadsPerStream  + threadBindingOffset} {
-//     }
-//     void on_scheduler_entry(bool) override {
-//         PinThreadToVacantCore(_offset + tbb::this_task_arena::current_thread_index(), _threadBindingStep, _ncpus, _mask);
-//     }
-//     void on_scheduler_exit(bool) override {
-//         PinCurrentThreadByMask(_ncpus, _mask);
-//     }
-//     void observe(bool state = true) {
-//         if (state) {
-//             _my_arena.initialize();
-//         }
-//         tbb::task_scheduler_observer::observe(state);
-//     }
-//     ~Observer() override = default;
-// };
+extern "C" {
+void __TBB_internal_initialize_system_topology(
+    std::size_t groups_num,
+    int& numa_nodes_count, int*& numa_indexes_list,
+    int& core_types_count, int*& core_types_indexes_list
+);
+class binding_handler;
+
+binding_handler* __TBB_internal_allocate_binding_handler(int number_of_slots, int numa_id, int core_type_id, int max_threads_per_core);
+void __TBB_internal_deallocate_binding_handler(binding_handler* handler_ptr);
+void __TBB_internal_apply_affinity(binding_handler* handler_ptr, int slot_num);
+void __TBB_internal_restore_affinity(binding_handler* handler_ptr, int slot_num);
+int __TBB_internal_get_default_concurrency(int numa_id, int core_type_id, int max_threads_per_core);
+}
+
+int get_processors_group_num() {
+#if defined(_WIN32) || defined(_WIN64)
+    SYSTEM_INFO si;
+    GetNativeSystemInfo(&si);
+
+    DWORD_PTR pam, sam, m = 1;
+    GetProcessAffinityMask(GetCurrentProcess(), &pam, &sam);
+    int nproc = 0;
+    for (std::size_t i = 0; i < sizeof(DWORD_PTR) * CHAR_BIT; ++i, m <<= 1) {
+        if ( pam & m )
+            ++nproc;
+    }
+    if (nproc == static_cast<int>(si.dwNumberOfProcessors)) {
+        return GetActiveProcessorGroupCount();
+    }
+#endif
+    return 1;
+}
+
+static int  numa_nodes_count = 0;
+static int* numa_nodes_indexes = nullptr;
+
+static int  core_types_count = 0;
+static int* core_types_indexes = nullptr;
+
+bool is_binding_environment_valid() {
+#if defined(_WIN32) && !defined(_WIN64)
+    static bool result = [] {
+        // For 32-bit Windows applications, process affinity masks can only support up to 32 logical CPUs.
+        SYSTEM_INFO si;
+        GetNativeSystemInfo(&si);
+        if (si.dwNumberOfProcessors > 32) return false;
+        return true;
+    }();
+    return result;
+#else
+    return true;
+#endif /* _WIN32 && !_WIN64 */
+}
+
+void initialize_system_topology() {
+    static std::once_flag is_topology_initialized;
+
+    std::call_once(is_topology_initialized, [&]{
+        if (is_binding_environment_valid()) {
+            __TBB_internal_initialize_system_topology(
+                get_processors_group_num(),
+                numa_nodes_count, numa_nodes_indexes,
+                core_types_count, core_types_indexes);
+        } else {
+            static int dummy_index = tbb::task_arena::automatic;
+
+            numa_nodes_count = 1;
+            numa_nodes_indexes = &dummy_index;
+
+            core_types_count = 1;
+            core_types_indexes = &dummy_index;
+        }
+    });
+}
+
+//! Returns the index, aka slot number, of the calling thread in its current arena
+inline int current_thread_index() {
+    int idx = tbb::task_arena::current_thread_index();
+    return idx == -1 ? tbb::task_arena::not_initialized : idx;
+}
+
+struct Observer: public tbb::task_scheduler_observer {
+    tbb::task_arena&    _my_arena;
+    binding_handler* my_binding_handler;
+    Observer(tbb::task_arena&    arena) :
+        tbb::task_scheduler_observer(arena),
+        _my_arena(arena) {
+            initialize_system_topology();
+    }
+    void on_scheduler_entry(bool) override {
+        __TBB_internal_apply_affinity(my_binding_handler, tbb::this_task_arena::current_thread_index());
+    }
+    void on_scheduler_exit(bool) override {
+        __TBB_internal_restore_affinity(my_binding_handler, tbb::this_task_arena::current_thread_index());
+    }
+    void observe(bool state = true) {
+        if (state) {
+            _my_arena.initialize();
+        }
+        tbb::task_scheduler_observer::observe(state);
+    }
+    Observer::~Observer() {
+        __TBB_internal_deallocate_binding_handler(my_binding_handler);
+    }
+};
 
 void compile_graph::run(program_impl& p) {
     OV_ITT_SCOPED_TASK(itt::domains::CLDNN, "CLDNN::pass::CompileGraph");
@@ -69,6 +147,8 @@ void compile_graph::run(program_impl& p) {
 #if (CLDNN_THREADING == CLDNN_THREADING_TBB)
     const auto n_threads = p.get_engine().configuration().n_threads;
     auto arena = std::unique_ptr<tbb::task_arena>(new tbb::task_arena());
+    auto observer = std::unique_ptr<Observer>(new Observer(*arena));
+    observer->observe(true);
     arena->initialize(n_threads);
     arena->execute([this, &p] {
         auto& proc_order = p.get_processing_order();
