9f8f35ddea4003cc1bc4e674b5656d296d4bba3f
Vladislav Volkov <vladislav.volkov@intel.com>
2021-10-27 15:31:08 +0300

    Migrating to the new tbbbind library version 2.5

--- inference-engine/src/inference_engine/src/threading/ie_parallel_custom_arena.cpp
+++ inference-engine/src/inference_engine/src/threading/ie_parallel_custom_arena.cpp
@@ -6,11 +6,11 @@
 
 #if IE_THREAD == IE_THREAD_TBB || IE_THREAD == IE_THREAD_TBB_AUTO
 
-#    ifndef TBBBIND_2_4_AVAILABLE
-#        define TBBBIND_2_4_AVAILABLE 0
+#    ifndef TBBBIND_2_5_AVAILABLE
+#        define TBBBIND_2_5_AVAILABLE 0
 #    endif
 
-#    define USE_TBBBIND_2_4                 (TBBBIND_2_4_AVAILABLE && TBB_INTERFACE_VERSION < 12020)
+#    define USE_TBBBIND_2_5                 (TBBBIND_2_5_AVAILABLE && TBB_INTERFACE_VERSION < 12020)
 #    define TBB_NUMA_SUPPORT_PRESENT        (TBB_INTERFACE_VERSION >= 11100)
 #    define TBB_HYBRID_CPUS_SUPPORT_PRESENT (TBB_INTERFACE_VERSION >= 12020)
 
@@ -21,7 +21,7 @@
 namespace custom {
 namespace detail {
 
-#    if USE_TBBBIND_2_4
+#    if USE_TBBBIND_2_5
 extern "C" {
 void __TBB_internal_initialize_system_topology(std::size_t groups_num,
                                                int& numa_nodes_count,
@@ -131,7 +131,7 @@ binding_oberver_ptr construct_binding_observer(tbb::task_arena& ta, int num_slot
     return observer;
 }
 
-#    endif /*USE_TBBBIND_2_4*/
+#    endif /*USE_TBBBIND_2_5*/
 
 #    if TBB_NUMA_SUPPORT_PRESENT
 tbb::task_arena::constraints convert_constraints(const custom::task_arena::constraints& c) {
@@ -154,7 +154,7 @@ task_arena::task_arena(int max_concurrency_, unsigned reserved_for_masters)
       my_binding_observer{} {}
 
 task_arena::task_arena(const constraints& constraints_, unsigned reserved_for_masters)
-#    if USE_TBBBIND_2_4
+#    if USE_TBBBIND_2_5
     : my_task_arena {
     info::default_concurrency(constraints_), reserved_for_masters
 }
@@ -177,7 +177,7 @@ task_arena::task_arena(const task_arena& s)
 
 void task_arena::initialize() {
     my_task_arena.initialize();
-#    if USE_TBBBIND_2_4
+#    if USE_TBBBIND_2_5
     std::call_once(my_initialization_state, [this] {
         my_binding_observer =
             detail::construct_binding_observer(my_task_arena, my_task_arena.max_concurrency(), my_constraints);
@@ -187,7 +187,7 @@ void task_arena::initialize() {
 
 void task_arena::initialize(int max_concurrency_, unsigned reserved_for_masters) {
     my_task_arena.initialize(max_concurrency_, reserved_for_masters);
-#    if USE_TBBBIND_2_4
+#    if USE_TBBBIND_2_5
     std::call_once(my_initialization_state, [this] {
         my_binding_observer =
             detail::construct_binding_observer(my_task_arena, my_task_arena.max_concurrency(), my_constraints);
@@ -197,7 +197,7 @@ void task_arena::initialize(int max_concurrency_, unsigned reserved_for_masters)
 
 void task_arena::initialize(constraints constraints_, unsigned reserved_for_masters) {
     my_constraints = constraints_;
-#    if USE_TBBBIND_2_4
+#    if USE_TBBBIND_2_5
     my_task_arena.initialize(info::default_concurrency(constraints_), reserved_for_masters);
     std::call_once(my_initialization_state, [this] {
         my_binding_observer =
@@ -221,7 +221,7 @@ int task_arena::max_concurrency() {
 
 namespace info {
 std::vector<numa_node_id> numa_nodes() {
-#    if USE_TBBBIND_2_4
+#    if USE_TBBBIND_2_5
     detail::initialize_system_topology();
     std::vector<numa_node_id> node_indexes(detail::numa_nodes_count);
     std::memcpy(node_indexes.data(), detail::numa_nodes_indexes, detail::numa_nodes_count * sizeof(int));
@@ -234,7 +234,7 @@ std::vector<numa_node_id> numa_nodes() {
 }
 
 std::vector<core_type_id> core_types() {
-#    if USE_TBBBIND_2_4
+#    if USE_TBBBIND_2_5
     detail::initialize_system_topology();
     std::vector<numa_node_id> core_type_indexes(detail::core_types_count);
     std::memcpy(core_type_indexes.data(), detail::core_types_indexes, detail::core_types_count * sizeof(int));
@@ -250,7 +250,7 @@ int default_concurrency(task_arena::constraints c) {
     if (c.max_concurrency > 0) {
         return c.max_concurrency;
     }
-#    if USE_TBBBIND_2_4
+#    if USE_TBBBIND_2_5
     if (detail::is_binding_environment_valid()) {
         detail::initialize_system_topology();
         return detail::__TBB_internal_get_default_concurrency(c.numa_id, c.core_type, c.max_threads_per_core);
