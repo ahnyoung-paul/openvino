2dfd4fc26b2dc941226632665c9fa321461a9681
Ahn, Paul Y <paul.y.ahn@intel.com>
2021-11-22 18:16:11 +0900

    add div test

--- inference-engine/thirdparty/clDNN/tests/test_cases/fusings_gpu_test.cpp
+++ inference-engine/thirdparty/clDNN/tests/test_cases/fusings_gpu_test.cpp
@@ -619,8 +619,12 @@ class ConvFusingTest : public WeightsPrimitiveFusingTest<bc_test_params> {
 public:
     void execute(bc_test_params& p) {
         auto input_prim = get_mem(get_input_layout(p));
+        // std::cout << "START building not_fused network ... " << std::endl;
         network network_not_fused(this->engine, this->topology_non_fused, bo_not_fused);
+        // std::cout << "END.. building not_fused network ... " << std::endl;
+        // std::cout << "START building fused network ... " << std::endl;
         network network_fused(this->engine, this->topology_fused, bo_fused);
+        // std::cout << "END.. building fused network ... " << std::endl;
         network_fused.set_input_data("input", input_prim);
         network_not_fused.set_input_data("input", input_prim);
 
@@ -1054,8 +1058,8 @@ INSTANTIATE_TEST_SUITE_P(fusings_gpu, conv_fp32_multi_eltwise_4_clamp,
 #define ADD_OPS
 #ifdef ADD_OPS
 
-class conv_fp32_additional_eltwise_fusing : public ConvFusingTest {};
-// TEST_P(conv_fp32_additional_eltwise_fusing, subtract_pattern01) {
+class conv_fp32_eltwise_fusing_sub : public ConvFusingTest {};
+// TEST_P(conv_fp32_eltwise_fusing_sub, subtract_pattern01) {
 //     if (engine.get_device_info().supports_immad) {
 //         return;
 //     }
@@ -1074,14 +1078,15 @@ class conv_fp32_additional_eltwise_fusing : public ConvFusingTest {};
 //         eltwise("eltwise4_sum", "eltwise3_prod", "eltwise_data4", eltwise_mode::sum),
 //         reorder("reorder_bfyx", "eltwise4_sum", p.default_format, data_types::f32)
 //     );
-//     implementation_desc conv_impl = { format::b_fs_yx_fsv16, ""};
+//     // implementation_desc conv_impl = { format::b_fs_yx_fsv16, ""};
+//     implementation_desc conv_impl = { format::bfyx, ""};
 //     bo_fused.set_option(build_option::force_implementations({ {"conv_prim", conv_impl} }));
 
 //     tolerance = 1e-5f;
 //     execute(p);
 // }
 
-// TEST_P(conv_fp32_additional_eltwise_fusing, subtract_pattern02) {
+// TEST_P(conv_fp32_eltwise_fusing_sub, subtract_pattern02) {
 //     if (engine.get_device_info().supports_immad) {
 //         return;
 //     }
@@ -1100,14 +1105,14 @@ class conv_fp32_additional_eltwise_fusing : public ConvFusingTest {};
 //         eltwise("eltwise4_sub", "eltwise3_prod", "eltwise_data4", eltwise_mode::sub),
 //         reorder("reorder_bfyx", "eltwise4_sub", p.default_format, data_types::f32)
 //     );
-//     implementation_desc conv_impl = { format::b_fs_yx_fsv16, ""};
+//     implementation_desc conv_impl = { format::bfyx, ""};
 //     bo_fused.set_option(build_option::force_implementations({ {"conv_prim", conv_impl} }));
 
 //     tolerance = 1e-5f;
 //     execute(p);
 // }
 
-// TEST_P(conv_fp32_additional_eltwise_fusing, subtract_pattern03) {
+// TEST_P(conv_fp32_eltwise_fusing_sub, subtract_pattern03) {
 //     if (engine.get_device_info().supports_immad) {
 //         return;
 //     }
@@ -1126,69 +1131,14 @@ class conv_fp32_additional_eltwise_fusing : public ConvFusingTest {};
 //         eltwise("eltwise4_sum", "eltwise3_sub", "eltwise_data4", eltwise_mode::sum),
 //         reorder("reorder_bfyx", "eltwise4_sum", p.default_format, data_types::f32)
 //     );
-//     implementation_desc conv_impl = { format::b_fs_yx_fsv16, ""};
+//     implementation_desc conv_impl = { format::bfyx, ""};
 //     bo_fused.set_option(build_option::force_implementations({ {"conv_prim", conv_impl} }));
 
 //     tolerance = 1e-5f;
 //     execute(p);
 // }
 
-// TEST_P(conv_fp32_additional_eltwise_fusing, subtract_pattern04) {
-//     if (engine.get_device_info().supports_immad) {
-//         return;
-//     }
-
-//     auto p = GetParam();
-//     create_topologies(input_layout("input", get_input_layout(p)),
-//         data("eltwise_data1", get_mem(get_output_layout(p))),
-//         data("eltwise_data2", get_mem(get_output_layout(p))),
-//         data("eltwise_data4", get_mem(get_output_layout(p))),
-//         data("bias", get_mem(get_bias_layout(p))),
-//         data("weights", get_mem(get_weights_layout(p))),
-//         convolution("conv_prim", "input", { "weights" }, {"bias"}, p.groups, p.stride, p.pad, p.dilation),
-//         eltwise("eltwise1_sum", "conv_prim", "eltwise_data1", eltwise_mode::sum),
-//         eltwise("eltwise2_sub", "conv_prim", "eltwise1_sum", eltwise_mode::sub),
-//         eltwise("eltwise3_prod", "eltwise2_sub", "eltwise_data2", eltwise_mode::prod),
-//         eltwise("eltwise4_sum", "eltwise3_prod", "eltwise_data4", eltwise_mode::sum),
-//         reorder("reorder_bfyx", "eltwise4_sum", p.default_format, data_types::f32)
-//     );
-//     implementation_desc conv_impl = { format::b_fs_yx_fsv16, ""};
-//     bo_fused.set_option(build_option::force_implementations({ {"conv_prim", conv_impl} }));
-
-//     tolerance = 1e-5f;
-//     execute(p);
-// }
-//scale("scale", "eltwise4_sum", "scale_data", "scale_bias"),
-        // data("scale_bias", get_mem(get_bias_layout(p))),
-// TEST_P(conv_fp32_additional_eltwise_fusing, subtract_pattern05) {
-//     if (engine.get_device_info().supports_immad) {
-//         return;
-//     }
-
-//     auto p = GetParam();
-//     create_topologies(input_layout("input", get_input_layout(p)),
-//         data("eltwise_data1", get_mem(get_output_layout(p))),
-//         data("eltwise_data2", get_mem(get_output_layout(p))),
-//         data("scale_data", get_mem(get_per_channel_layout(p), 1.0f/p.kernel.count())),
-//         data("bias", get_mem(get_bias_layout(p))),
-//         data("weights", get_mem(get_weights_layout(p))),
-//         convolution("conv_prim", "input", { "weights" }, {"bias"}, p.groups, p.stride, p.pad, p.dilation),
-//         eltwise("eltwise1_sum", "conv_prim", "eltwise_data1", eltwise_mode::sum),
-//         eltwise("eltwise2_sub", "eltwise1_sum", "conv_prim", eltwise_mode::sub),
-//         eltwise("eltwise3_prod", "eltwise2_sub", "eltwise_data2", eltwise_mode::prod),
-//         eltwise("eltwise4_sum", "eltwise3_prod", "eltwise1_sum", eltwise_mode::sum),
-//         scale("scale", "eltwise4_sum", "scale_data"),
-//         reorder("reorder_bfyx", "scale", p.default_format, data_types::f32)
-//     );
-//     // implementation_desc conv_impl = { format::b_fs_yx_fsv16, ""};
-//     // bo_fused.set_option(build_option::force_implementations({ {"conv_prim", conv_impl} }));
-
-//     tolerance = 1e-5f;
-//     // tolerance = 1.0f;
-//     execute(p);
-// }
-
-TEST_P(conv_fp32_additional_eltwise_fusing, div_pattern01) {
+TEST_P(conv_fp32_eltwise_fusing_sub, subtract_pattern04) {
     if (engine.get_device_info().supports_immad) {
         return;
     }
@@ -1202,8 +1152,8 @@ TEST_P(conv_fp32_additional_eltwise_fusing, div_pattern01) {
         data("weights", get_mem(get_weights_layout(p))),
         convolution("conv_prim", "input", { "weights" }, {"bias"}, p.groups, p.stride, p.pad, p.dilation),
         eltwise("eltwise1_sum", "conv_prim", "eltwise_data1", eltwise_mode::sum),
-        eltwise("eltwise2_div", "conv_prim", "eltwise_data2", eltwise_mode::div),
-        eltwise("eltwise3_prod", "eltwise1_sum", "eltwise2_div", eltwise_mode::prod),
+        eltwise("eltwise2_sub", "conv_prim", "eltwise1_sum", eltwise_mode::sub),
+        eltwise("eltwise3_prod", "eltwise2_sub", "eltwise_data2", eltwise_mode::prod),
         eltwise("eltwise4_sum", "eltwise3_prod", "eltwise_data4", eltwise_mode::sum),
         reorder("reorder_bfyx", "eltwise4_sum", p.default_format, data_types::f32)
     );
@@ -1214,15 +1164,92 @@ TEST_P(conv_fp32_additional_eltwise_fusing, div_pattern01) {
     execute(p);
 }
 
-INSTANTIATE_TEST_SUITE_P(fusings_gpu, conv_fp32_additional_eltwise_fusing,
+INSTANTIATE_TEST_SUITE_P(fusings_gpu, conv_fp32_eltwise_fusing_sub,
                         ::testing::ValuesIn(std::vector<bc_test_params>{
                                              bc_test_params{CASE_CONV_FP32_2, 2, 6},
-                                            //  bc_test_params{CASE_CONV_FP32_3, 2, 6},
-                                            //  bc_test_params{CASE_CONV_FP32_4, 2, 6},
+                                             bc_test_params{CASE_CONV_FP32_3, 2, 6},
+                                             bc_test_params{CASE_CONV_FP32_4, 2, 6},
+
+                                             bc_test_params{CASE_CONV_FP16_2, 2, 6},
+                                             bc_test_params{CASE_CONV_FP16_3, 2, 6},
+                                             bc_test_params{CASE_CONV_FP16_4, 2, 6},
+                                             }));
+
+class conv_fp32_eltwise_fusing_sub_scale : public ConvFusingTest {};
+TEST_P(conv_fp32_eltwise_fusing_sub_scale, subtract_pattern05) {
+    if (engine.get_device_info().supports_immad) {
+        return;
+    }
+
+    auto p = GetParam();
+    create_topologies(input_layout("input", get_input_layout(p)),
+        data("eltwise_data1", get_mem(get_output_layout(p))),
+        data("eltwise_data2", get_mem(get_output_layout(p))),
+        data("scale_data", get_mem(get_per_channel_layout(p), 1.0f/p.kernel.count())),
+        data("bias", get_mem(get_bias_layout(p))),
+        data("weights", get_mem(get_weights_layout(p))),
+        convolution("conv_prim", "input", { "weights" }, {"bias"}, p.groups, p.stride, p.pad, p.dilation),
+        eltwise("eltwise1_sum", "conv_prim", "eltwise_data1", eltwise_mode::sum),
+        eltwise("eltwise2_sub", "eltwise1_sum", "conv_prim", eltwise_mode::sub),
+        eltwise("eltwise3_prod", "eltwise2_sub", "eltwise_data2", eltwise_mode::prod),
+        eltwise("eltwise4_sum", "eltwise3_prod", "eltwise1_sum", eltwise_mode::sum),
+        scale("scale", "eltwise4_sum", "scale_data"),
+        reorder("reorder_bfyx", "scale", p.default_format, data_types::f32)
+    );
+    implementation_desc conv_impl = { format::b_fs_yx_fsv16, ""};
+    bo_fused.set_option(build_option::force_implementations({ {"conv_prim", conv_impl} }));
+
+    tolerance = 1e-5f;
+    // tolerance = 1.0f;
+    execute(p);
+}
+
+INSTANTIATE_TEST_SUITE_P(fusings_gpu, conv_fp32_eltwise_fusing_sub_scale,
+                        ::testing::ValuesIn(std::vector<bc_test_params>{
+                                            //  bc_test_params{CASE_CONV_FP32_2, 2, 7},
+                                            //  bc_test_params{CASE_CONV_FP32_3, 2, 7},
+                                             bc_test_params{CASE_CONV_FP32_4, 2, 7},
 
                                             //  bc_test_params{CASE_CONV_FP16_2, 2, 6},
                                             //  bc_test_params{CASE_CONV_FP16_3, 2, 6},
-                                            //  bc_test_params{CASE_CONV_FP16_4, 2, 6},
+                                             bc_test_params{CASE_CONV_FP16_4, 2, 7},
+                                             }));
+
+class conv_fp32_eltwise_fusing_div : public ConvFusingTest {};
+TEST_P(conv_fp32_eltwise_fusing_div, div_pattern01) {
+    if (engine.get_device_info().supports_immad) {
+        return;
+    }
+
+    auto p = GetParam();
+    create_topologies(input_layout("input", get_input_layout(p)),
+        data("eltwise_data1", get_mem(get_output_layout(p))),
+        data("eltwise_data2", get_mem(get_output_layout(p), 1.0f)),
+        data("eltwise_data3", get_mem(get_output_layout(p))),
+        data("eltwise_data4", get_mem(get_output_layout(p))),
+        data("bias", get_mem(get_bias_layout(p))),
+        data("weights", get_mem(get_weights_layout(p))),
+        convolution("conv_prim", "input", { "weights" }, {"bias"}, p.groups, p.stride, p.pad, p.dilation),
+        eltwise("eltwise1_sum", "conv_prim", "eltwise_data1", eltwise_mode::sum),
+        eltwise("eltwise2_div", "eltwise1_sum", "eltwise_data2", eltwise_mode::div),
+        eltwise("eltwise3_prod", "eltwise2_div", "eltwise_data3", eltwise_mode::prod),
+        eltwise("eltwise4_sum", "eltwise3_prod", "eltwise_data4", eltwise_mode::sum),
+        reorder("reorder_bfyx", "eltwise4_sum", p.default_format, data_types::f32)
+    );
+    implementation_desc conv_impl = { format::bfyx, ""};
+    bo_fused.set_option(build_option::force_implementations({ {"conv_prim", conv_impl} }));
+
+    tolerance = 1e-5f;
+    execute(p);
+}
+
+INSTANTIATE_TEST_SUITE_P(fusings_gpu, conv_fp32_eltwise_fusing_div,
+                        ::testing::ValuesIn(std::vector<bc_test_params>{
+                                             bc_test_params{CASE_CONV_FP32_2, 2, 6},
+                                             bc_test_params{CASE_CONV_FP32_3, 2, 6},
+
+                                             bc_test_params{CASE_CONV_FP16_2, 2, 6},
+                                             bc_test_params{CASE_CONV_FP16_3, 2, 6},
                                              }));
 
 
