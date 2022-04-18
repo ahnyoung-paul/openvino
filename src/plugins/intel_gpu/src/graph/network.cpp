bd32e08b20ff00a94502c87a825cfe50c08d4e2a
Ahn, Paul Y <paul.y.ahn@intel.com>
2022-04-18 21:53:13 +0900

    [GPU][DynamicShape] Fix code for optimization
    - Only realloc memory when new layout size is bigger than max layout size
    - Keep Reshape's input constant data on usm_host when shape is changed
    - Fix issue new out memory is not set to _output in set_output_memory()

--- src/plugins/intel_gpu/src/graph/network.cpp
+++ src/plugins/intel_gpu/src/graph/network.cpp
@@ -7,6 +7,7 @@
 #include "intel_gpu/primitives/data.hpp"
 #include "intel_gpu/primitives/mutable_data.hpp"
 #include "intel_gpu/primitives/input_layout.hpp"
+#include "intel_gpu/primitives/reshape.hpp"
 
 #include "intel_gpu/runtime/error_handler.hpp"
 #include "intel_gpu/runtime/memory.hpp"
@@ -876,6 +877,20 @@ void network::allocate_primitive_instance(program_node const& node) {
         if (node.is_type<data>())
             _data_outputs.push_back(inst);
     }
+
+    auto transfer_memory_to_host = [&](std::shared_ptr<primitive_inst> instance, program_node const& node) {
+        auto& inst_mem = instance->output_memory();
+        auto alloc_type = inst_mem.get_allocation_type();
+
+        if (alloc_type == allocation_type::usm_device) {
+            // Allocate and transfer memory
+            auto host_mem = inst_mem.get_engine()->allocate_memory(inst_mem.get_layout(), allocation_type::usm_host, false);
+            host_mem->copy_from(get_stream(), inst_mem);
+            _memory_pool->release_memory(&inst_mem, node.id(), get_id());
+            instance->set_output_memory(host_mem);
+        }
+    };
+
     if (node.is_constant())
         transfer_memory_to_device(inst, node);
 }
@@ -884,6 +899,12 @@ void network::transfer_memory_to_device(std::shared_ptr<primitive_inst> instance
     OV_ITT_SCOPED_TASK(itt::domains::CLDNN, "NetworkImpl::TransferMemory");
     auto& inst_mem = instance->output_memory();
     auto alloc_type = inst_mem.get_allocation_type();
+
+    auto users = node.get_users();
+    if (users.size() == 1
+        && users.front()->is_type<reshape>()
+        && users.front()->is_dynamic())
+            return;
 
     // Do not transfer memory if a user requires lockable memory.
     // If memory is used in both gpu and cpu implementations, primitive itself is responsible for correct allocation type
