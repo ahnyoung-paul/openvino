66e23281258b643dfa7d11a074d1384ba2fd24f4
Ahn, Paul Y <paul.y.ahn@intel.com>
2022-12-23 17:03:57 +0900

    Apply multi-threads to async compilation

--- src/plugins/intel_gpu/src/graph/compilation_context.cpp
+++ src/plugins/intel_gpu/src/graph/compilation_context.cpp
@@ -12,20 +12,26 @@ class CompilationContext : public ICompilationContext {
 public:
     using compilation_queue_t = InferenceEngine::ThreadSafeQueue<ICompilationContext::Task>;
 
+    CompilationContext(CompilationContext&& other) = default;
+
     CompilationContext(cldnn::engine& engine, size_t program_id) {
-        _kernels_cache = cldnn::make_unique<kernels_cache>(engine, program_id, kernel_selector::KernelBase::get_db().get_batch_header_str());
-        _worker = std::thread([this](){
-            while (!_stop_compilation) {
-                CompilationContext::Task task;
-                bool success = _queue.try_pop(task);
-                if (success) {
-                    task(*_kernels_cache);
-                } else {
-                    std::chrono::milliseconds ms{1};
-                    std::this_thread::sleep_for(ms);
+        const size_t max_num_threads = 8;
+        for (size_t i = 0; i < max_num_threads; i++) {
+            auto thread = std::thread([this, &program_id, &engine](){
+                auto m_kernels_cache = cldnn::make_unique<kernels_cache>(engine, program_id, kernel_selector::KernelBase::get_db().get_batch_header_str());
+                while (!_stop_compilation) {
+                    CompilationContext::Task task;
+                    bool success = _queue.try_pop(task);
+                    if (success) {
+                        task(*m_kernels_cache);
+                    } else {
+                        std::chrono::milliseconds ms{1};
+                        std::this_thread::sleep_for(ms);
+                    }
                 }
-            }
-        });
+            });
+            _workers.push_back(std::move(thread));
+        }
     }
 
     void push_task(ICompilationContext::Task&& task) override {
@@ -34,8 +40,10 @@ public:
 
     void cancel() noexcept override {
         _stop_compilation = true;
-        if (_worker.joinable())
-            _worker.join();
+        for (size_t i = 0; i < _workers.size(); i++) {
+            if (_workers[i].joinable())
+                _workers.at(i).join();
+        }
     }
 
     ~CompilationContext() noexcept { cancel(); }
@@ -43,7 +51,7 @@ public:
 private:
     std::unique_ptr<kernels_cache> _kernels_cache;
     compilation_queue_t _queue;
-    std::thread _worker;
+    std::vector<std::thread> _workers;
     std::atomic_bool _stop_compilation{false};
 };
 
