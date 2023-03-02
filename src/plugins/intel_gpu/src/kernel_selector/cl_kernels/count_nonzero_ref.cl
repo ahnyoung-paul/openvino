b8b1020a9f19ee8ecdf7f65433f22947340b74b7
Ahn, Paul Y <paul.y.ahn@intel.com>
2023-03-02 17:49:02 +0900

    made 2 kernels

--- src/plugins/intel_gpu/src/kernel_selector/cl_kernels/count_nonzero_ref.cl
+++ src/plugins/intel_gpu/src/kernel_selector/cl_kernels/count_nonzero_ref.cl
@@ -6,11 +6,12 @@
 
 #define INPUT0_GET_INDEX1(idx_order) INPUT0_GET_INDEX(idx_order)
 
-KERNEL (count_nonzero_ref)(
+#if COUNT_NONZERO_PARITAL_SUM
+KERNEL (count_nonzero_ref_parital_sum)(
     OPTIONAL_SHAPE_INFO_ARG
     const __global INPUT0_TYPE* input,
     volatile __global uint* tmp_buffer,
-    volatile __global OUTPUT_TYPE* output)
+    volatile __global uint* buf_length)
 {
     const uint gdim0 = (uint)get_global_id(0);
     const uint gdim1 = (uint)get_global_id(1);
@@ -19,7 +20,7 @@ KERNEL (count_nonzero_ref)(
     const uint ldim1 = (uint)get_local_id(1);
     const uint ldim2 = (uint)get_local_id(2);
     const uint group_id = (uint)get_group_id(2);
-    local uint local_count;
+    __local uint local_count;
 
 
     if (ldim0 == 0 && ldim1 == 0 && ldim2 == 0) {
@@ -51,6 +52,7 @@ KERNEL (count_nonzero_ref)(
     uint count = (input[INPUT0_GET_INDEX1(INPUT_ORDER)] == INPUT0_VAL_ZERO) ? 0 : 1;
     count = sub_group_reduce_add(count);
 
+    sub_group_barrier(0);
     if (get_sub_group_local_id() == 0) {
         atomic_add(&local_count, count);
         // printf("** nonzero_count[Step01][%3d] : local_count[%3d] = count %3d(%3d) - subgroup_size(%3d)\n", get_global_size(2), group_id, local_count, count, get_sub_group_size());
@@ -58,25 +60,41 @@ KERNEL (count_nonzero_ref)(
 
     work_group_barrier(CLK_GLOBAL_MEM_FENCE | CLK_LOCAL_MEM_FENCE);
     if (ldim0 == 0 && ldim1 == 0 && ldim2 == 0) {
-        tmp_buffer[group_id] = count;
+        tmp_buffer[group_id] = local_count;
         // printf("** nonzero_count[Step02][%3d] : tmp_buffer[%3d] = %3d(%3d) - subgroup_size(%3d)\n", get_global_size(2), group_id, tmp_buffer[group_id], local_count, get_sub_group_size());
     }
+    *buf_length = (uint)get_num_groups(2);
 
-    work_group_barrier(CLK_GLOBAL_MEM_FENCE | CLK_LOCAL_MEM_FENCE);
-    if (gdim0 == 0 && gdim1 == 0 && gdim2 == 0) {
-        output[0] = 0;
-        uint group_size = (uint)get_num_groups(2);
-        for (uint gid = 0; gid < group_size; gid++ ) {
-            output[0] = output[0] + tmp_buffer[gid];
-            // const uint cgid = gid;
-            // printf("nonzero_count - GlobalWG[%3d,%3d,%3d],LocalWG[%3d,%3d,%3d],group_size[%3d],get_global_size(%3d)tem_buffer[%3d](%3d),output[%3d]\n",gdim0, gdim1, gdim2, ldim0, ldim1, ldim2, group_size, get_global_size(2), cgid, tmp_buffer[cgid], output[0]);
-        }
-    }
+    // work_group_barrier(CLK_GLOBAL_MEM_FENCE | CLK_LOCAL_MEM_FENCE);
+    // if (gdim0 == 0 && gdim1 == 0 && gdim2 == 0) {
+    //     output[0] = 0;
+    //     uint group_size = (uint)get_num_groups(2);
+    //     for (uint gid = 0; gid < group_size; gid++ ) {
+    //         output[0] = output[0] + tmp_buffer[gid];
+    //         // const uint cgid = gid;
+    //         // printf("nonzero_count - GlobalWG[%3d,%3d,%3d],LocalWG[%3d,%3d,%3d],group_size[%3d],get_global_size(%3d)tem_buffer[%3d](%3d),output[%3d]\n",gdim0, gdim1, gdim2, ldim0, ldim1, ldim2, group_size, get_global_size(2), cgid, tmp_buffer[cgid], output[0]);
+    //     }
+    // }
 
 // printf("nonzero_count - GlobalWG[%3d,%3d,%3d],LocalWG[%3d,%3d,%3d],Subgroup[%3d][D: %3d] -- get_global_size[%3d,%3d,%3d], get_local_size[%3d,%3d,%3d], get_num_groups[%3d,%3d,%3d], get_group_id[%3d,%3d,%3d]\n",
 //             gdim0, gdim1, gdim2, local0, local1, local2, get_sub_group_local_id(), shape_info[0],
 //             get_global_size(0),get_global_size(1),get_global_size(2),get_local_size(0),get_local_size(1),get_local_size(2),get_num_groups(0),get_num_groups(1),get_num_groups(2),get_group_id(0),get_group_id(1),get_group_id(2));
 }
 
+#else
+KERNEL (count_nonzero_ref_final)(
+    OPTIONAL_SHAPE_INFO_ARG
+    volatile __global uint* tmp_buffer,
+    volatile __global uint* buf_length,
+    volatile __global OUTPUT_TYPE* output)
+{
+    output[0] = 0;
+    const uint buf_size = buf_length[0];
+    for (uint gid = 0; gid < buf_size; gid++ ) {
+        output[0] = output[0] + tmp_buffer[gid];
+        // printf("nonzero_count_sum - tem_buffer[%3d / %3d](%3d),output[%3d]\n", gid, buf_size, tmp_buffer[gid], output[0]);
+    }
+}
+#endif // COUNT_NONZERO_PARITAL_SUM
 #undef INPUT0_GET_INDEX1
 #undef INPUT_ORDER
--- src/plugins/intel_gpu/src/kernel_selector/kernels/non_zero/count_nonzero_kernel_ref.cpp
+++ src/plugins/intel_gpu/src/kernel_selector/kernels/non_zero/count_nonzero_kernel_ref.cpp
@@ -33,57 +33,36 @@ ParamsKey CountNonzeroKernelRef::GetSupportedKey() const {
     return k;
 }
 #define USE_ORIGINAL
-CommonDispatchData CountNonzeroKernelRef::SetDefault(const count_nonzero_params& params) const {
-    CommonDispatchData dispatchData;
+CountNonzeroKernelRef::MultiDispatchData CountNonzeroKernelRef::SetDefault(const count_nonzero_params& params) const {
+    MultiDispatchData dispatchData;
     const auto& input = params.inputs[0];
-#ifdef USE_ORIGINAL
     auto in_layout = params.inputs[0].GetLayout();
     auto out_layout = params.outputs[0].GetLayout();
-#endif
     std::vector<std::vector<Tensor::DataChannelName>> dims_by_gws;
 
     int rank = input.Dimentions();
     if (rank == 4) {
-        dispatchData.gws = {input.X().v, input.Y().v, input.Feature().v * input.Batch().v};
+        dispatchData.stage_1.gws = {input.X().v, input.Y().v, input.Feature().v * input.Batch().v};
         dims_by_gws = {{Tensor::DataChannelName::X},
                        {Tensor::DataChannelName::Y},
                        {Tensor::DataChannelName::FEATURE, Tensor::DataChannelName::BATCH}};
     } else if (rank == 5) {
-        dispatchData.gws = {input.X().v, input.Y().v * input.Z().v, input.Feature().v * input.Batch().v};
+        dispatchData.stage_1.gws = {input.X().v, input.Y().v * input.Z().v, input.Feature().v * input.Batch().v};
         dims_by_gws = {{Tensor::DataChannelName::X},
                        {Tensor::DataChannelName::Y, Tensor::DataChannelName::Z},
                        {Tensor::DataChannelName::FEATURE, Tensor::DataChannelName::BATCH}};
     } else {
-        dispatchData.gws = {input.X().v * input.Y().v, input.Z().v * input.W().v, input.Feature().v * input.Batch().v};
+        dispatchData.stage_1.gws = {input.X().v * input.Y().v, input.Z().v * input.W().v, input.Feature().v * input.Batch().v};
         dims_by_gws = {{Tensor::DataChannelName::X, Tensor::DataChannelName::Y},
                        {Tensor::DataChannelName::Z, Tensor::DataChannelName::W},
                        {Tensor::DataChannelName::FEATURE, Tensor::DataChannelName::BATCH}};
     }
 
-#ifdef USE_ORIGINAL
-    dispatchData.lws =
-        GetOptimalLocalWorkGroupSizes(dispatchData.gws, params.engineInfo, in_layout, out_layout, dims_by_gws);
-#else
-    dispatchData.lws = dispatchData.gws;
-#endif
+    dispatchData.stage_1.lws =
+        GetOptimalLocalWorkGroupSizes(dispatchData.stage_1.gws, params.engineInfo, in_layout, out_layout, dims_by_gws);
 
-    // size_t sum = 1;
-    // for (auto& v : dispatchData.gws) {
-    //     sum = sum * v;
-    // }
-
-    // if (sum == 56) {
-    //     std::cout << "dispatchData.gws={";
-    //     for (auto& v : dispatchData.gws) {
-    //         std::cout << v << ",";
-    //     }
-    //     std::cout << "}" << std::endl;
-    //     std::cout << "dispatchData.lws={";
-    //     for (auto& v : dispatchData.lws) {
-    //         std::cout << v << ",";
-    //     }
-    //     std::cout << "}" << std::endl;
-    // }
+    dispatchData.stage_final.gws = { 1, 1, 1};
+    dispatchData.stage_final.lws = {1, 1, 1};
 
     return dispatchData;
 }
@@ -99,52 +78,65 @@ DeviceFeaturesKey CountNonzeroKernelRef::get_required_device_features_key(const 
 KernelsData CountNonzeroKernelRef::GetKernelsData(const Params& params, const optional_params& options) const {
     assert(params.GetType() == KernelType::COUNT_NONZERO);
 
-    KernelData kd = KernelData::Default<count_nonzero_params>(params);
+    constexpr size_t kernels_num = 2;
+    KernelData kd = KernelData::Default<count_nonzero_params>(params, kernels_num);
     count_nonzero_params& newParams = *static_cast<count_nonzero_params*>(kd.params.get());
 
     auto dispatchData = SetDefault(newParams);
-    auto entry_point = GetEntryPoint(kernelName, newParams.layerID, params, options);
-    auto cldnn_jit = MakeBaseParamsJitConstants(newParams);
-    auto jit = CreateJit(kernelName, cldnn_jit, entry_point);
-
-    auto& kernel = kd.kernels[0];
-    bool is_dynamic = newParams.inputs[0].is_dynamic();
 
     kd.update_dispatch_data_func = [this](const Params& params, KernelData& kd) {
         const auto& prim_params = static_cast<const count_nonzero_params&>(params);
         auto dispatchData = SetDefault(prim_params);
-        OPENVINO_ASSERT(kd.kernels.size() == 1, "[GPU] Invalid kernels size for update dispatch data func");
-        auto& updated_kernel = kd.kernels[0];
-        updated_kernel.params.workGroups.global = dispatchData.gws;
-        updated_kernel.params.workGroups.local = dispatchData.lws;
+        OPENVINO_ASSERT(kd.kernels.size() == 2, "[GPU] Invalid kernels size for update dispatch data func");
 
-        auto& gws = updated_kernel.params.workGroups.global;
-        auto& lws = updated_kernel.params.workGroups.local;
+        // update gws, lws for stage 1
+        auto& updated_kernel_stage1 = kd.kernels[0];
+        updated_kernel_stage1.params.workGroups.global = dispatchData.stage_1.gws;
+        updated_kernel_stage1.params.workGroups.local = dispatchData.stage_1.lws;
+
+        // update gws, lws for stage final
+        auto& updated_kernel_stage_final = kd.kernels[1];
+        updated_kernel_stage_final.params.workGroups.global = dispatchData.stage_final.gws;
+        updated_kernel_stage_final.params.workGroups.local = dispatchData.stage_final.lws;
+
+        // update internal buffer size
+        auto& gws = updated_kernel_stage1.params.workGroups.global;
+        auto& lws = updated_kernel_stage1.params.workGroups.local;
         size_t gws_mul = std::accumulate(gws.begin(), gws.end(), 1, std::multiplies<size_t>());
         size_t lws_mul = std::accumulate(lws.begin(), lws.end(), 1, std::multiplies<size_t>());
 
-        size_t buffer_size = static_cast<size_t>(std::ceil(static_cast<double>(gws_mul) / lws_mul)) * sizeof(size_t) * 2;
+        size_t buffer_size = static_cast<size_t>(std::ceil(static_cast<double>(gws_mul) / lws_mul)) * sizeof(uint32_t) * 2;
         kd.internalBufferSizes.clear();
         kd.internalBufferSizes.push_back(buffer_size);
+        kd.internalBufferSizes.push_back(1 * sizeof(uint32_t));
         kd.internalBufferDataType = kernel_selector::Datatype::UINT32;
     };
 
-    // In case of count-nonzero, the output shape is static unconditionally,
-    // so it should be checked as dynamic of the input shape
-    FillCLKernelData(kernel,
-                     dispatchData,
-                     params.engineInfo,
-                     kernelName,
-                     jit,
-                     entry_point,
-                     "",
-                     false,
-                     false,
-                     1,
-                     GetFusedPrimitiveInputsCount(params),
-                     1,
-                     is_dynamic);
+    bool is_dynamic = newParams.inputs[0].is_dynamic();
     {
+        auto entry_point = GetEntryPoint(kernelName, newParams.layerID, params, options);
+        auto cldnn_jit = MakeBaseParamsJitConstants(newParams);
+        cldnn_jit.AddConstant(MakeJitConstant("COUNT_NONZERO_PARITAL_SUM", 1));
+        auto jit = CreateJit(kernelName, cldnn_jit, entry_point);
+
+        auto& kernel = kd.kernels[0];
+
+        // In case of count-nonzero, the output shape is static unconditionally,
+        // so it should be checked as dynamic of the input shape
+        FillCLKernelData(kernel,
+                        dispatchData.stage_1,
+                        params.engineInfo,
+                        kernelName,
+                        jit,
+                        entry_point,
+                        "",
+                        false,
+                        false,
+                        1,
+                        GetFusedPrimitiveInputsCount(params),
+                        1,
+                        is_dynamic);
+
         auto& args = kernel.params.arguments;
         args.clear();
         if (is_dynamic) {
@@ -152,16 +144,53 @@ KernelsData CountNonzeroKernelRef::GetKernelsData(const Params& params, const op
         }
         args.push_back({ArgumentDescriptor::Types::INPUT, 0});
         args.push_back({ArgumentDescriptor::Types::INTERNAL_BUFFER, 0});
-        args.push_back({ArgumentDescriptor::Types::OUTPUT, 0});
+        args.push_back({ArgumentDescriptor::Types::INTERNAL_BUFFER, 1});
+    }
+    {
+        auto entry_point = GetEntryPoint(kernelName, newParams.layerID, params, options, 1);
+        auto cldnn_jit = MakeBaseParamsJitConstants(newParams);
+        auto jit = CreateJit(kernelName, cldnn_jit, entry_point);
 
+        auto& kernel = kd.kernels[1];
+
+        // In case of count-nonzero, the output shape is static unconditionally,
+        // so it should be checked as dynamic of the input shape
+        FillCLKernelData(kernel,
+                        dispatchData.stage_final,
+                        params.engineInfo,
+                        kernelName,
+                        jit,
+                        entry_point,
+                        "",
+                        false,
+                        false,
+                        1,
+                        GetFusedPrimitiveInputsCount(params),
+                        1,
+                        is_dynamic);
+
+        auto& args = kernel.params.arguments;
+        args.clear();
+        if (is_dynamic) {
+            args.push_back({ArgumentDescriptor::Types::SHAPE_INFO, 0});
+        }
+        args.push_back({ArgumentDescriptor::Types::INTERNAL_BUFFER, 0});
+        args.push_back({ArgumentDescriptor::Types::INTERNAL_BUFFER, 1});
+        args.push_back({ArgumentDescriptor::Types::OUTPUT, 0});
+    }
+
+    // update internal buffer size
+    {
+        auto& kernel = kd.kernels[0];
         auto& gws = kernel.params.workGroups.global;
         auto& lws = kernel.params.workGroups.local;
         size_t gws_mul = std::accumulate(gws.begin(), gws.end(), 1, std::multiplies<size_t>());
         size_t lws_mul = std::accumulate(lws.begin(), lws.end(), 1, std::multiplies<size_t>());
 
-        size_t buffer_size = static_cast<size_t>(std::ceil(static_cast<double>(gws_mul) / lws_mul)) * sizeof(size_t) * 2;
+        size_t buffer_size = static_cast<size_t>(std::ceil(static_cast<double>(gws_mul) / lws_mul)) * sizeof(uint32_t) * 2;
         kd.internalBufferSizes.clear();
         kd.internalBufferSizes.push_back(buffer_size);
+        kd.internalBufferSizes.push_back(1 * sizeof(uint32_t));
         kd.internalBufferDataType = kernel_selector::Datatype::UINT32;
     }
     return {kd};
