1518c2890d8e4a493dbb296b3a210605e85c047d
Ahn, Paul Y <paul.y.ahn@intel.com>
2026-03-27 13:06:30 +0900

    [Temp] modify debug code

--- src/plugins/intel_gpu/src/graph/debug_helper.cpp
+++ src/plugins/intel_gpu/src/graph/debug_helper.cpp
@@ -44,9 +44,9 @@ size_t get_x_pitch(const layout& layout) {
 }
 
 template <class T>
-void __validate_data_range(memory::ptr mem, stream& stream, const layout& data_layout, std::string &info) {
+bool __validate_data_range(memory::ptr mem, stream& stream, const layout& data_layout, std::string &info) {
     if (!mem)
-        return;
+        return true;
 
     // Reinterpret buffer to represent actual data layout (same as log_memory_to_file)
     auto actual_mem = mem->get_engine()->reinterpret_buffer(*mem, data_layout);
@@ -66,7 +66,7 @@ void __validate_data_range(memory::ptr mem, stream& stream, const layout& data_l
             if (std::isinf(val) || std::isnan(val)) {
                 std::string err_str = std::isinf(val) ? "inf" : "nan";
                 GPU_DEBUG_COUT << err_str << " WAS FOUND: " << info << "  *********************" << std::endl;
-                return;
+                return false;
             }
             if (val > val_max)
                 val_max = val;
@@ -86,9 +86,9 @@ void __validate_data_range(memory::ptr mem, stream& stream, const layout& data_l
                                 for (ov::Dimension::value_type x = 0; x < size.spatial[0]; ++x, input_it += x_pitch) {
                                     auto val = convert_element(mem_ptr[input_it]);
                                     if (std::isinf(val) || std::isnan(val)) {
-                                        std::string err_str = std::isinf(val) ? "inf" : "nan";
-                                        GPU_DEBUG_COUT << err_str << " WAS FOUND: " << info << "  *********************" << std::endl;
-                                        return;
+                                        // std::string err_str = std::isinf(val) ? "inf" : "nan";
+                                        // GPU_DEBUG_COUT << err_str << " WAS FOUND: " << info << "  *********************" << std::endl;
+                                        return false;
                                     }
                                     if (val > val_max)
                                         val_max = val;
@@ -103,20 +103,89 @@ void __validate_data_range(memory::ptr mem, stream& stream, const layout& data_l
         }
     }
     GPU_DEBUG_INFO << "min, max = " << val_min << ", " << val_max << "  : " << info << "  is_packed " << is_memory_packed << std::endl;
+    return true;
 }
 
-void validate_data_range(memory::ptr mem, stream& stream, const layout& data_layout, std::string &info) {
+bool validate_data_range(memory::ptr mem, stream& stream, const layout& data_layout, std::string &info) {
     auto data_type = data_layout.data_type;
     if (data_type == cldnn::data_types::f32)
-        __validate_data_range<float>(mem, stream, data_layout, info);
+        return __validate_data_range<float>(mem, stream, data_layout, info);
     else if (data_type == cldnn::data_types::f16)
-        __validate_data_range<ov::float16>(mem, stream, data_layout, info);
+        return __validate_data_range<ov::float16>(mem, stream, data_layout, info);
     else if (data_type == cldnn::data_types::i8)
-        __validate_data_range<int8_t>(mem, stream, data_layout, info);
+        return __validate_data_range<int8_t>(mem, stream, data_layout, info);
     else if (data_type == cldnn::data_types::u8)
-        __validate_data_range<uint8_t>(mem, stream, data_layout, info);
+        return __validate_data_range<uint8_t>(mem, stream, data_layout, info);
     else
         GPU_DEBUG_INFO << "Unsupport data type for validating data range " << data_type << std::endl;
+    return true;
+}
+
+struct DataRangeInfo {
+    float min_val = std::numeric_limits<float>::max();
+    float max_val = std::numeric_limits<float>::lowest();
+    bool has_nan = false;
+    bool has_inf = false;
+};
+
+template <class T>
+DataRangeInfo __get_data_range(memory::ptr mem, stream& stream, const layout& data_layout) {
+    DataRangeInfo result;
+    if (!mem) return result;
+    auto actual_mem = mem->get_engine()->reinterpret_buffer(*mem, data_layout);
+    auto&& size = actual_mem->get_layout().get_tensor();
+    mem_lock<T, mem_lock_type::read> lock(actual_mem, stream);
+    auto mem_ptr = lock.data();
+    auto x_pitch = get_x_pitch(actual_mem->get_layout());
+    const bool is_memory_packed = !actual_mem->is_memory_reset_needed(actual_mem->get_layout());
+
+    auto process_val = [&](float val) {
+        if (std::isnan(val)) { result.has_nan = true; return; }
+        if (std::isinf(val)) { result.has_inf = true; return; }
+        if (val > result.max_val) result.max_val = val;
+        if (val < result.min_val) result.min_val = val;
+    };
+    auto found_bad = [&]() { return result.has_nan || result.has_inf; };
+
+    if (is_memory_packed) {
+        for (size_t i = 0; i < actual_mem->count() && !found_bad(); ++i) {
+            process_val(convert_element(mem_ptr[i]));
+        }
+    } else {
+        for (ov::Dimension::value_type g = 0; g < size.group[0] && !found_bad(); ++g) {
+            for (ov::Dimension::value_type b = 0; b < size.batch[0] && !found_bad(); ++b) {
+                for (ov::Dimension::value_type f = 0; f < size.feature[0] && !found_bad(); ++f) {
+                    for (ov::Dimension::value_type w = 0; w < size.spatial[3] && !found_bad(); ++w) {
+                        for (ov::Dimension::value_type z = 0; z < size.spatial[2] && !found_bad(); ++z) {
+                            for (ov::Dimension::value_type y = 0; y < size.spatial[1] && !found_bad(); ++y) {
+                                cldnn::tensor t(cldnn::group(g), cldnn::batch(b), cldnn::feature(f), cldnn::spatial(0, y, z, w));
+                                size_t input_it = actual_mem->get_layout().get_linear_offset(t);
+                                for (ov::Dimension::value_type x = 0; x < size.spatial[0] && !found_bad(); ++x, input_it += x_pitch) {
+                                    process_val(convert_element(mem_ptr[input_it]));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return result;
+}
+
+DataRangeInfo get_data_range(memory::ptr mem, stream& stream, const layout& data_layout) {
+    DataRangeInfo result;
+    if (!mem) return result;
+    auto data_type = data_layout.data_type;
+    if (data_type == cldnn::data_types::f32)
+        return __get_data_range<float>(mem, stream, data_layout);
+    else if (data_type == cldnn::data_types::f16)
+        return __get_data_range<ov::float16>(mem, stream, data_layout);
+    else if (data_type == cldnn::data_types::i8)
+        return __get_data_range<int8_t>(mem, stream, data_layout);
+    else if (data_type == cldnn::data_types::u8)
+        return __get_data_range<uint8_t>(mem, stream, data_layout);
+    return result;
 }
 
 template <class T>
@@ -504,12 +573,39 @@ NodeDebugHelper::NodeDebugHelper(const primitive_inst& inst)
 NodeDebugHelper::~NodeDebugHelper() {
     const auto& config = m_network.get_config();
 
-    if (config.get_validate_output_buffer() && !m_network.is_internal()) {
+    if (config.get_validate_output_buffer() && !m_network.is_internal() && (m_network.get_id() == 4)) {
         m_stream.finish(); // Wait for stream completion before checking output buffers
         for (size_t i = 0; i < m_inst.outputs_memory_count(); i++) {
             auto output_mem = m_inst.output_memory_ptr(i);
-            std::string info = m_inst.id() + "(" + std::to_string(i) + ") at iteration " + std::to_string(m_network.get_current_iteration_num());
-            validate_data_range(output_mem, m_stream, m_inst.get_output_layout(i), info);
+            std::string info = m_inst.id() + "(" + std::to_string(i) + ") at iteration " + std::to_string(m_network.get_current_iteration_num())
+                    + " net_id " + std::to_string(m_network.get_id());
+            bool valid = validate_data_range(output_mem, m_stream, m_inst.get_output_layout(i), info);
+            if (!valid || m_inst.id().find("Result_") != std::string::npos) {
+                // Print input buffer data range
+                {
+                    auto out_range = get_data_range(output_mem, m_stream, m_inst.get_output_layout(i));
+                    std::string err_type = out_range.has_nan ? "NaN"
+                    : ( out_range.has_inf ? "INF" : "[" + std::to_string(out_range.min_val) + ", " + std::to_string(out_range.max_val) + "]" );
+                    std::stringstream ss;
+                    ss << "Found " << err_type << " " << m_inst.id()
+                                    << " = input(" << m_inst.dependencies().size() << "), m_net_id " << m_network.get_id()
+                                    << ", m_iter " << m_network.get_current_iteration_num() << std::endl;
+                    for (size_t j = 0; j < m_inst.dependencies().size(); ++j) {
+                        auto dep = m_inst.dependencies().at(j);
+                        auto input_mem = m_inst.dep_memory_ptr(j);
+                        auto input_layout = dep.first->get_output_layout(dep.second);
+                        auto range = get_data_range(input_mem, m_stream, input_layout);
+                        if (range.has_nan)
+                            ss << "* IN[" << j << "]: " << dep.first->id() << " [NaN]" << std::endl;
+                        else if (range.has_inf)
+                            ss << "* IN[" << j << "]: " << dep.first->id() << " [INF]" << std::endl;
+                        else
+                            ss << "* IN[" << j << "]: " << dep.first->id()
+                                            << ": [" << range.min_val << ", " << range.max_val << "]" << std::endl;
+                    }
+                    GPU_DEBUG_COUT << ss.str();
+                }
+            }
         }
     }
 
